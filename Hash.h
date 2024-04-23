9ba0058c471a7f46630aec29435a94aa4fde428b
And1deng <Asdeng03@gmail.com>
2024-04-22 22:23:37 -0700

    Added Hash.cpp & .h

---
+++ Hash.h
@@ -0,0 +1,9 @@
+#ifndef HASHPIN_H_
+#define HASHPIN_H_
+
+#include <iostream>
+using namespace std;
+
+int hashPIN(int input);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 // void passwordMgr();
+//test
 
 int main() {
 
